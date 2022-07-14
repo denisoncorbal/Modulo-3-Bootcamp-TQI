f1686272f2e69dba170db6c37551454083945076
Denison G. Corbal <denison.corbal@gmail.com>
2022-07-14 15:29:15 -0300

    Adicionado temporizador, verificador de vitória e níveis de dificuldade

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+﻿// função de tempo para completar o jogo
+
+function mode(){
+   lockBoard = true;
+   let facil = document.createElement("button");
+   facil.textContent = "Fácil";
+   facil.setAttribute("onclick", "timer(3)");
+   facil.setAttribute("class", "button");
+
+   let medio = document.createElement("button");
+   medio.textContent = "Médio";
+   medio.setAttribute("onclick", "timer(2)");
+   medio.setAttribute("class", "button");
+
+   let dificil = document.createElement("button");
+   dificil.textContent = "Difícil";
+   dificil.setAttribute("onclick", "timer(1)");
+   dificil.setAttribute("class", "button");
+   
+   
+   let divMode = document.getElementById('mode');
+   divMode.innerText = "Escolha a dificuldade";
+   divMode.appendChild(facil);
+   divMode.appendChild(medio);
+   divMode.appendChild(dificil);
+}
+
+function timer(minutes){
+   lockBoard = false;
+  document.getElementById('mode').style.display = "none";  
+
+  let seconds = 0;
+
+  let interval = setInterval(function(){
+    seconds = seconds - 1;
+    if(seconds < 0){
+      seconds = 59;
+      minutes = minutes - 1;
+    }
+  if(minutes < 0){
+    clearInterval(interval);
+     alert("O tempo acabou!");
+     document.location.reload(true);
+  }
+    document.getElementById('timer').innerText = "Tempo: " + minutes + ":" + seconds;
+  }, 1000);
+
+}
+
+function checkWinning(){
+   let win = true;
+
+   cards.forEach((card) => {
+        if(!card.classList.contains('flip'))
+           win = false;
+   })
+
+   if(win){
+      alert("Você venceu!");
+      document.location.reload(true);
+   }
+}
+
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
@@ -18,6 +81,8 @@ function flipCard() {
     secondCard = this;
     hasFlippedCard = false;
     checkForMatch();
+
+    checkWinning();
 }
 
 //função que checa se as cartas são iguais
